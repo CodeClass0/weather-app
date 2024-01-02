89ff4d91464ab7b98ee283e0bd8c7f81ead35622
C A <chadandari@outlook.com>
2024-01-02 18:27:35 -0500

    Current weather icon works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,14 @@ var currentTemp = document.querySelector(".cardCurrent-temp");
 var currentWind = document.querySelector(".cardCurrent-wind");
 var currentHumidity = document.querySelector(".cardCurrent-humidity");
 var currentDate = document.querySelector(".cardCurrent-date");
-
+var currentIcon = document.querySelector(".cardCurrent-ico");
 cardObj1 = {
     title: document.querySelector(".card1-title"),
     Date: document.querySelector(".card1-date"),
     temp: document.querySelector(".card1-temp"),
     wind: document.querySelector(".card1-wind"),
     humidity: document.querySelector(".card1-humidity"),
+    icon: document.querySelector(".card1-ico"),
 };
 cardObj2 = {
     title: document.querySelector(".card2-title"),
@@ -18,6 +19,7 @@ cardObj2 = {
     temp: document.querySelector(".card2-temp"),
     wind: document.querySelector(".card2-wind"),
     humidity: document.querySelector(".card2-humidity"),
+    icon: document.querySelector(".card2-ico"),
 };
 cardObj3 = {
     title: document.querySelector(".card3-title"),
@@ -25,6 +27,7 @@ cardObj3 = {
     temp: document.querySelector(".card3-temp"),
     wind: document.querySelector(".card3-wind"),
     humidity: document.querySelector(".card3-humidity"),
+    icon: document.querySelector(".card3-ico"),
 };
 cardObj4 = {
     title: document.querySelector(".card4-title"),
@@ -32,6 +35,7 @@ cardObj4 = {
     temp: document.querySelector(".card4-temp"),
     wind: document.querySelector(".card4-wind"),
     humidity: document.querySelector(".card4-humidity"),
+    icon: document.querySelector(".card4-ico"), 
 };
 cardObj5 = {
     title: document.querySelector(".card5-title"),
@@ -39,9 +43,9 @@ cardObj5 = {
     temp: document.querySelector(".card5-temp"),
     wind: document.querySelector(".card5-wind"),
     humidity: document.querySelector(".card5-humidity"),
+    icon: document.querySelector(".card5-ico"),
 };
 var cardArray = [cardObj1,cardObj2,cardObj3,cardObj4,cardObj5];
-
 
 function buildCurrentWeather(city){
     var citySearch = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=" + key;
@@ -57,6 +61,7 @@ function buildCurrentWeather(city){
         fetch (currentWeatherCall)
         .then((response) => response.json())
         .then((data)=>{
+            console.log(data);
             currentDate.textContent = location + " (" + dayjs().format("MM/DD/YYYY") + ")";
             var temp = Math.round(((data.main.temp - 273.15)* (9/5)+32) * 100)/100;
             console.log("Temperature: " + temp + "°F");
@@ -65,6 +70,9 @@ function buildCurrentWeather(city){
             currentWind.textContent = "Wind: " + data.wind.speed + " MPH";
             console.log ("Humidity: " + data.main.humidity + " %");
             currentHumidity.textContent = "Humidity: " + data.main.humidity + " %";
+            console.log(data.weather.icon);
+            var url = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            $('.cardCurrent-ico').attr('src',url);
         })
     })
 }
@@ -102,6 +110,8 @@ function buildForecast(city){
                 console.log(data.list[i].wind.speed);
                 cardArray[i].humidity.textContent= "Humidity: " + data.list[i].main.humidity + "%";
                 console.log(data.list[i].main.humidity);
+                // cardArray[i].icon.textContent = data.list[i].weather.icon;
+                // console.log(data.list[i].weather.icon);
             }
         })
     })
